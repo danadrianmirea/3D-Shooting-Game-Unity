050f4d90dc03aea44d154833f79e5308c279d383
Dan Adrian Mirea <damirea84@gmail.com>
2025-02-25 16:05:53 +0200

    remove music

---
+++ Assets/AudioDebugger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioDebugger : MonoBehaviour
+{
+    void Start()
+    {
+        DebugAllAudioSources();
+    }
+
+    void DebugAllAudioSources()
+    {
+        AudioSource[] allSources = Resources.FindObjectsOfTypeAll<AudioSource>();
+
+        foreach (var source in allSources)
+        {
+            string clipName = (source.clip != null) ? source.clip.name : "None";
+            Debug.Log("AudioSource found on: " + source.gameObject.name + ", Clip: " + clipName);
+        }
+    }
+}
